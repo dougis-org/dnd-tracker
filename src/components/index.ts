d5ce1d4b63e6c482ccdb17be46d7124e521516c9
Doug Hubbard <doug@dougis.com>
2025-11-03 07:12:02 -0800

    chore(task): complete T003 add component exports entrypoint

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Centralized exports for shared UI components.
+export { ThemeProvider } from './theme/theme-provider'
+export { ThemeToggle } from './theme/theme-toggle'
+export { Button, buttonVariants } from './ui/button'
+
+// Navigation-related components will be exported as they are implemented.
